a5e9889569a0f64f6471c6594ac35cdfcfc43a9d
Ethan D'Lima <e.dlima@mail.utoronto.ca>
2021-03-23 10:59:21 -0400

    finished most functionality for web app

--- pub/js/example.js
+++ pub/js/example.js
@@ -1,10 +1,90 @@
 /* JS Library usage examples */
 "use strict";
 
-const g = new Graphersal('myGraph')
-g.addNode(100, 30)
+const g1 = new Graphersal('graph1')
+document.querySelector('#BFS').appendChild(g1.getGraphElement())
+
+function generateGraph(g, n) {
+    while (g.nodes.length > 0) {
+        g.removeNode(0)
+    }
+
+    for (let i = 0; i < n; i++) {
+        g.addNode(Math.random()*360+20, Math.random()*160+20)
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i+1; j < n; j++) {
+            parseInt(Math.random()*2) ? g.addEdge(i,j) : null;
+        }
+    }
+}
+
+function runBFS(g) {
+    let s = g.getNode(0)
+    const queue = []
+    queue.push([s,1])
+    g.setFound(s,1)
+    while (queue.length != 0) {
+        const q = queue.shift()
+        q[0].neigbours.forEach(edge => {
+            if (!edge.node.found) {
+                queue.push([edge.node, q[1]+1])
+                g.setFound(edge.node, q[1]+1)
+            }
+        });
+    }
+}
+
+function reset(g) {
+    g.resetGraph()
+}
+
+
+/* g.addNode(100, 30)
 g.addNode(70, 110)
-g.addEdge(g.nodes[0], g.nodes[1])
+g.addEdge(0, 1)
 g.addNode(310, 50)
-g.addEdge(g.nodes[1], g.nodes[2])
+g.addEdge(1, 2) */
 //g.colorNode(g.nodes[1], 'chartreuse')
+
+const g2 = new Graphersal('graph2')
+document.querySelector('#colors').appendChild(g2.getGraphElement())
+function changeGraphColor(g, color) {
+    g.nodes.forEach(node => g.colorNode(node, color))
+}
+
+
+
+
+
+
+const g3 = new Graphersal('graph3')
+document.querySelector('#edgeCut').appendChild(g3.getGraphElement())
+function changeGraphColor(g, color) {
+    g.nodes.forEach(node => g.colorNode(node, color))
+}
+
+function addListeners() {
+    let p = g3.getGraphElement().createSVGPoint(); 
+    let q = g3.getGraphElement().createSVGPoint(); 
+
+    g3.getGraphElement().addEventListener('mousedown', (e) => {
+        if (e.target.id = g3.getGraphElement().id) {
+            p.x = e.clientX;
+            p.y = e.clientY;
+            p =  p.matrixTransform(g3.getGraphElement().getScreenCTM().inverse());
+        }
+    });
+    //g3.getGraphElement().addEventListener('mousemove', drag);
+    g3.getGraphElement().addEventListener('mouseup', (e) => {
+        q.x = e.clientX;
+        q.y = e.clientY;
+        q =  q.matrixTransform(g3.getGraphElement().getScreenCTM().inverse());
+        console.log(p)
+        console.log(q)
+        g3.cutEdges(p.x, p.y, q.x, q.y)
+    });
+    //g3.getGraphElement().addEventListener('mouseleave', endDrag);
+}
+addListeners()
--- pub/js/graphersal.js
+++ pub/js/graphersal.js
@@ -42,7 +42,7 @@ function _removeNode(node, graph) {
     
     graph.textElements.removeChild(node.textElement)
     graph.nodeElements.removeChild(node.element)
-    graph.nodes = graph.nodes.filter(p => p != node)
+    graph.nodes = graph.nodes.filter(p => p !== node)
     graph.nodes.forEach((node, i) => {
         node.textElement.innerHTML = `${i+1}`
     })
@@ -57,31 +57,68 @@ function _setEdge(node1, node2) {
 
     let edgeElement = node1.neigbours[index].edgeElement
 
+    let x1, y1, x2, y2;
     if (node1.neigbours[index].first) {
-        edgeElement.setAttribute('x1', `${node1.element.cx.baseVal.value}`)
-        edgeElement.setAttribute('y1', `${node1.element.cy.baseVal.value}`)
-        edgeElement.setAttribute('x2', `${node2.element.cx.baseVal.value}`)
-        edgeElement.setAttribute('y2', `${node2.element.cy.baseVal.value}`)
-    } 
-    else {
-        edgeElement.setAttribute('x1', `${node2.element.cx.baseVal.value}`)
-        edgeElement.setAttribute('y1', `${node2.element.cy.baseVal.value}`)
-        edgeElement.setAttribute('x2', `${node1.element.cx.baseVal.value}`)
-        edgeElement.setAttribute('y2', `${node1.element.cy.baseVal.value}`)
+        x1 = node1.element.cx.baseVal.value
+        y1 = node1.element.cy.baseVal.value
+        x2 = node2.element.cx.baseVal.value
+        y2 = node2.element.cy.baseVal.value
     }
+    else {
+        x1 = node2.element.cx.baseVal.value
+        y1 = node2.element.cy.baseVal.value
+        x2 = node1.element.cx.baseVal.value
+        y2 = node1.element.cy.baseVal.value
+    }
+
+    let norm = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+    let d1 = (x2 - x1) / norm
+    let d2 = (y2 - y1) / norm
+
+
+    edgeElement.setAttribute('x1', `${x1+20*d1}`)
+    edgeElement.setAttribute('y1', `${y1+20*d2}`)
+    edgeElement.setAttribute('x2', `${x2-20*d1}`)
+    edgeElement.setAttribute('y2', `${y2-20*d2}`)  
     
 }
 
+function _orientation(p, q, r) { 
+    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/ 
+    // for details of below formula. 
+    let val = (q[1] - p[1]) * (r[0] - q[0]) - 
+              (q[0] - p[0]) * (r[1] - q[1]); 
+  
+    if (val == 0) return 0;  // colinear 
+  
+    return (val > 0)? 1: 2; // clock or counterclock wise 
+} 
+
+function _doIntersect(p1, q1, p2, q2) { 
+    // Find the four orientations needed for general and 
+    // special cases 
+    let o1 = _orientation(p1, q1, p2); 
+    let o2 = _orientation(p1, q1, q2); 
+    let o3 = _orientation(p2, q2, p1); 
+    let o4 = _orientation(p2, q2, q1); 
+  
+    // General case 
+    return o1 != o2 && o3 != o4;
+
+}
+
 // Main library code
-function Graphersal(graphID) {
+function Graphersal(graphID, height, width) {
 	// the constructor function should instantiate any variables that
 	//  each Circle Generator instance should have a unique version of.
 	//  In this case, each CG should have its own array of circles separate from
 	//  other CGs.
     this.graphElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    this.graphElement.style = 'position: relative; width: 400px; height: 200px;'
+    const h = height ? height : 200;
+    const w = width ? width : 400
+    this.graphElement.style = `position: relative; width: ${w}px; height: ${h}px;`
     this.graphElement.id = graphID
-    document.querySelector('body').append(this.graphElement)
+    //document.querySelector('body').append(this.graphElement)
 
     this.textElements = document.createElementNS('http://www.w3.org/2000/svg', 'g')
     this.edgeElements = document.createElementNS('http://www.w3.org/2000/svg', 'g');
@@ -101,8 +138,9 @@ function Graphersal(graphID) {
     this.graphElement.addEventListener('mouseup', endDrag);
     this.graphElement.addEventListener('mouseleave', endDrag);
 
-    const graphElement = this.graphElement
-    const nodes = this.nodes
+    /* const graphElement = this.graphElement
+    const nodes = this.nodes */
+    const g = this
     function getMousePosition(evt) {
         return {
           x: evt.clientX,
@@ -128,15 +166,15 @@ function Graphersal(graphID) {
             selectedElement.setAttribute('cy', `${dragY - offset.y}`)
 
             // updating edge positions
-            let index = nodes.findIndex(node => {
+            let index = g.nodes.findIndex(node => {
                 return node.element === selectedElement
             })
-            nodes[index].neigbours.forEach(edge => {
-                _setEdge(nodes[index], edge.node)
+            g.nodes[index].neigbours.forEach(edge => {
+                _setEdge(g.nodes[index], edge.node)
             });
             //update text position
-            nodes[index].textElement.setAttribute('x', `${dragX - offset.x}`)
-            nodes[index].textElement.setAttribute('y', `${dragY - offset.y + 5}`)
+            g.nodes[index].textElement.setAttribute('x', `${dragX - offset.x}`)
+            g.nodes[index].textElement.setAttribute('y', `${dragY - offset.y + 5}`)
 
         }
     }
@@ -144,13 +182,14 @@ function Graphersal(graphID) {
         selectedElement = null;
     }
 
-    this.graphElement.addEventListener('mousedown', (e) => {
+    // User can add this themselves
+    /* this.graphElement.addEventListener('mousedown', (e) => {
         if (e.button == 2) {
             e.preventDefault()
             console.log('right clicked')
             _addNode(e.clientX, e.clientY, this)
         }
-    });
+    }); */
 
     //window.oncontextmenu = function(e) { e.preventDefault();}
 }
@@ -161,8 +200,9 @@ Graphersal.prototype = {
         _addNode(cx, cy, this)
 	},
 
-    addEdge: function(node1, node2) {
-        
+    addEdge: function(index1, index2) {
+        const node1 = this.nodes[index1]
+        const node2 = this.nodes[index2]
         let index = node1.neigbours.findIndex(obj => {
             return obj.node === node2
         })
@@ -192,8 +232,8 @@ Graphersal.prototype = {
     // i is the order in which the node is found
     // 1 if found first, 2 if found second, ...
     setFound(node, i) {
+        node.found = true
         setTimeout(() => {
-            node.found = true
             node.element.setAttribute('fill', 'chartreuse')
         }, 1000*i)
 
@@ -215,9 +255,39 @@ Graphersal.prototype = {
         })
     },
 
-    removeNode(node) {
-        _removeNode(node, this)
-    }
+    removeNode(index) {
+        _removeNode(this.nodes[index], this)
+    },
+    getGraphElement() {
+        return this.graphElement
+    },
+    getNode(index) {
+        return this.nodes[index]
+    },
+    setDimensions(h, w) {
+        h ? this.graphElement.style["height"] = `${h}px` : null;
+        w ? this.graphElement.style["width"] = `${w}px` : null;
+    },
+    // removes edges across the line segement 
+    cutEdges(x1, y1, x2, y2) {
+        const p1 = [x1,y1]
+        const q1 = [x2,y2]
+        this.nodes.forEach((node) => {
+            node.neigbours = node.neigbours.filter((edge) => {
+                let p2 = [edge.edgeElement.x1.baseVal.value, edge.edgeElement.y1.baseVal.value]
+                let q2 = [edge.edgeElement.x2.baseVal.value, edge.edgeElement.y2.baseVal.value]
+
+                const intersect = _doIntersect(p1, q1, p2, q2)
+                if (intersect) {
+                    edge.node.neigbours = edge.node.neigbours.filter(e => e.node !== node)
+                    this.edgeElements.removeChild(edge.edgeElement)
+                }
+                console.log(!intersect)
+                return !intersect
+            })
+        })
+    } 
+
 
 }
 
